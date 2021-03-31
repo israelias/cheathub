f16b19438adf30cb5d354240270d343596c57d83
israelias <israelias.js@gmail.com>
2021-03-31 17:49:00 +0800

    Develop constants and type definitions

--- frontend/src/react-app-env.d.ts
+++ frontend/src/react-app-env.d.ts
@@ -1,1 +1,26 @@
 /// <reference types="react-scripts" />
+
+interface Snippet {
+  [id: string]: any,
+  title: string;
+  language: string;
+  addedBy: string;
+  likedBy: string[];
+  addedOn: string;
+  updatedOn: string;
+  description: string;
+  value: string;
+  tags: string[];
+  collection: string[];
+}
+
+type searchBy = "title" | "language" | "tags" ;
+type searchTerm = string;
+
+type test = keyof typeof Snippet;
+type test2 = typeof Snippet[test];
+
+interface Options {
+  value: string;
+  label: string;
+}
