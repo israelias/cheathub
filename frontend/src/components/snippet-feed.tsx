afa50d0e6b4654344c531bdddf47bb70b22a3e6d
israelias <israelias.js@gmail.com>
2021-04-01 01:30:24 +0800

    Compose gist-feed container element

--- frontend/src/components/snippet-feed.tsx
+++ frontend/src/components/snippet-feed.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Tags } from './shared/tags'
-import { searchBy, searchTerm} from './snippet_crud/search-form'
+import { Tags } from './shared/tags';
+import { Likes } from './shared/liked-by'
 import { string } from 'yup/lib/locale'
 
 interface snippetFeedProps {
@@ -10,85 +10,66 @@ interface snippetFeedProps {
   username: String,
 }
 
-interface snippetFeedPropsState {
-
-}
-
 export const SnippetFeed: React.FC<snippetFeedProps> = ({
   snippets,
   searchBy,
   searchTerm,
   username,
 }) => {
-  const [tags, setTags] = React.useState('');
-  const [searchParams, setSearchParams] = React.useState('');
-  const [searchText, setSearchText] = React.useState(searchTerm);
-  const [snippetsBase, setSnippetsBase] = React.useState(snippets);
-
-
-  snippets.map((snippet) => {
-    searchTerm && setSearchText(typeof(searchTerm) === 'string' ? searchTerm.toLowerCase() : searchTerm);
-    snippet.tags && setTags(snippet.tags.join(''));
-    snippet[searchBy] && setSearchParams(searchBy.toLowerCase());
-
-    if (snippet[searchBy].toLowerCase().indexOf(searchText) !== -1) {
-      return (
-        <div>
-          <h2>{snippet.title}</h2>
-          { username === snippet.addedBy && <a href={`/edit/${snippet.id}`}>EDIT</a>}
-          <h3>{snippet.language}</h3>
-          <h4><a href={`/snippets/user/${snippet.addedBy}`}>{snippet.addedBy}</a> - {snippet.addedOn}</h4>
-          <section id="snippet-code">
-            <pre>{snippet.value}</pre>
-          </section>
-          <section id="snippet-notes">
-            <p>{snippet.description}</p>
-          </section>
-          { snippet.tags.length > 0 && snippet.tags[0] !== '' &&
-            <Tags
-              tags={snippet.tags}
-            />
-          }
-        </div>
-      )
-    }
-
-  })
-    return (
-      <>
+  // const [tags, setTags] = React.useState('');
+  // const [searchParams, setSearchParams] = React.useState('');
+  // const [searchText, setSearchText] = React.useState(searchTerm);
+  // const [snippetsBase, setSnippetsBase] = React.useState(snippets);
+    // searchTerm && setSearchText(typeof(searchTerm) === 'string' ? searchTerm.toLowerCase() : searchTerm);
+    // snippet.tags && setTags(snippet.tags.join(''));
+    // snippet[searchBy] && setSearchParams(searchBy.toLowerCase());
+  return (
+    <>
       {
         snippets.map((snippet) => (
-          {
-            if (snippet.tags){
-              setParsedTags(snippet.tags.join(''))
-            };
-            if (snippet[searchBy].toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1) {
-
-            }
-
-          }
+          snippet[searchBy].indexOf(searchTerm) !== -1
+          &&
           (
             <div>
-              <h2>snippet.title</h2>
-              { user === snippet.creater && <a href="/edit/snippet.id">EDIT</a>}
-              <h3>snippet.language</h3>
-              <h4><a href="/snippets/user/snippet.creator %>">{snippet.creator}</a> - {snippet.createdOn}</h4>
-          <section id="snippet-code">
-            <pre>{snippet.valie}</pre>
-          </section>
-          <section id="snippet-notes">
-            <p>{snippet.description}</p>
-          </section>
-          {
-            snippet.tags.length > 0 && snippet.tags[0] !== '' &&
-            <Tags
-                tags={snippet.tags}
-              />
-          }
+              <h2>{snippet.title}</h2>
+              {
+                username === snippet.addedBy
+                &&
+                <a href={`/edit/${snippet.id}`}>
+                  EDIT
+                </a>
+              }
+              <h3>{snippet.language}</h3>
+              <h4>
+                <a href={`/snippets/user/${snippet.addedBy}`}>
+                  {snippet.addedBy}
+                </a> - {snippet.addedOn}
+              </h4>
+              <section id="snippet-code">
+                <pre>{snippet.value}</pre>
+              </section>
+              <section id="snippet-notes">
+                <p>{snippet.description}</p>
+              </section>
+              {
+                snippet.tags.length > 0
+                && snippet.tags[0] !== ''
+                &&
+                <Tags
+                  tags={snippet.tags}
+                />
+              }
+              {
+                snippet.likedBy.length > 0
+                &&
+                <Likes
+                  usernames={snippet.likedBy}
+                />
+              }
             </div>
           )
         ))
       }
-      </>
-    );
+    </>
+  );
 }
