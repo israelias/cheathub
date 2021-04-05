f3668b091ab693a67d67969e97e5f06a2432a060
israelias <israelias.js@gmail.com>
2021-04-05 22:17:21 +0800

    Deconstruct fetch variety in lib directory

---
+++ frontend/src/lib/fetcher.ts
@@ -0,0 +1,132 @@
+import { RouterProps, RouteProps, RouteComponentProps, RouteChildrenProps } from "react-router-dom";
+import fetch from "isomorphic-unfetch";
+import { RequestTicket } from "./requests"
+
+interface ISignUpRequest extends UserContext, RouterProps {
+  redirectTo: string;
+  body: {
+    username: string;
+    email: string;
+    password: string;
+  }
+}
+
+export function signUpRequest({ setAccessToken, setUsername, setLoggedIn, setLoading, history: router, redirectTo, body }: ISignUpRequest) {
+  const request = RequestTicket({
+    method: 'post',
+    url: 'api/auth/signup',
+    body: {username: body.username, email: body.email, password: body.password}
+  })
+  setLoading(true)
+  return fetch(request)
+    .then(res => res.json())
+    .then(data => {
+      if (data.access_token) {
+        setAccessToken(data.access_token)
+        setUsername(data.username)
+        setLoggedIn(true)
+        localStorage.setItem('username', data.username)
+        setLoading(false)
+        router.push({
+          pathname: redirectTo + data.username,
+        })
+      }
+    })
+}
+
+interface ILoginRequest extends UserContext, RouteComponentProps {
+  redirectTo: string;
+  body: {
+    email: string;
+    password: string;
+  }
+}
+
+export function loginRequest({ body, setAccessToken, setUsername, setLoggedIn, history, redirectTo }: ILoginRequest
+  ) {
+  const request = RequestTicket({
+    method: 'post',
+    url: 'api/auth/login',
+    body: {email: body.email, password: body.password}
+  })
+  return fetch(request).then(r => r.json())
+    .then(token => {
+      if (token.access_token) {
+        setAccessToken(token.access_token)
+        setUsername(token.username)
+        setLoggedIn(true)
+        localStorage.setItem('username', token.username)
+        history.push({
+          pathname: redirectTo + token.username,
+        })
+      }
+    })
+}
+
+interface ILogoutRequest extends UserContext, RouteProps, RouterProps {
+  redirectTo: string;
+  body: {
+    email: string;
+    password: string;
+  }
+}
+
+export function logoutRequest({ setUsername, setLoggedIn, setAccessToken, accessToken, history: router, redirectTo }: ILogoutRequest) {
+  const request = RequestTicket({
+    method: 'post',
+    url: 'api/auth/logout',
+    token: accessToken
+  })
+  return fetch(request).then(res => {
+    if (res.ok) {
+      const now = Date.now()
+      setAccessToken('')
+      setUsername('')
+      setLoggedIn(false)
+      const outgoing = window.localStorage.getItem('username')
+      window.localStorage.removeItem('username')
+      window.localStorage.setItem('app_logout', now.toString())
+      router.push({
+        pathname: redirectTo,
+        // query: {name: outgoing}
+      });
+    }
+  })
+    .catch(error => console.error(error));
+}
+
+interface IPutRequest extends UserContext {
+  url: string;
+  body: object;
+}
+
+export function putRequest({ url, accessToken, body }: IPutRequest) {
+  const request = RequestTicket({
+    method: 'put',
+    url: url,
+    token: accessToken,
+    body: body
+  })
+  return fetch(request)
+    .then(res => res.ok
+    ? res.json()
+    : console.log(res))
+    .catch(error => console.error(error));
+}
+
+interface IGetRequest extends UserContext {
+  url: string;
+}
+
+export function getRequest({ url, accessToken }: IGetRequest) {
+  const request = RequestTicket({
+    method: 'get',
+    url: url,
+    token: accessToken ? accessToken : '',
+  })
+  return fetch(request)
+    .then(res => res.ok
+      ? res.json()
+      : console.log(res))
+    .catch(error => console.error(error));
+}
---
+++ frontend/src/lib/requests.ts
@@ -0,0 +1,56 @@
+import fetch from "isomorphic-unfetch";
+
+const API = 'http://localhost:5000'
+
+interface IRequestTicket {
+  method: string;
+  token?: string;
+  url: string;
+  body?: object;
+}
+
+export const RequestTicket = ({ method, token, url, body }: IRequestTicket) => {
+  if (method === 'post') {
+    if (token) {
+      return new Request(`${API}/${url}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': token ? `Bearer ${token}` : ''
+        },
+        credentials: 'include',
+        body: body ? JSON.stringify(body) : null
+      });
+    }
+    else {
+      return new Request(`${API}/${url}`, {
+        method: "POST",
+        credentials: 'include',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(body),
+      })
+    }
+  }
+
+  if (method === 'put') {
+    return new Request(`${API}/${url}`, {
+      method: "PUT",
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify(body)
+    })
+  }
+  else {
+    return new Request(`${API}/${url}`, {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+        'Authorization': token ? `Bearer ${token}` : ''
+      },
+    })
+  }
+}
