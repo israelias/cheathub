0b0641dd1c4e0b3cc421643e996e2bc05c01537e
israelias <israelias.js@gmail.com>
2021-04-05 23:19:54 +0800

    Add context wrapper for user data

---
+++ frontend/src/context/user.context.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+
+const UserContext = React.createContext<UserContext | null>({} as UserContext);
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const UserProvider = ({ children }: Props) => {
+  console.log("First provider")
+  const [username, setUsername] = React.useState('');
+  const [accessToken, setAccessToken] = React.useState('');
+  const [loggedIn, setLoggedIn] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    function checkUserData(e: StorageEvent) {
+      console.log("TRIGGER")
+
+      if (e.key === 'app_logout') {
+        console.log("TRIGGER LOGOUT")
+        window.alert("You have been logged out.")
+      }
+    }
+
+    window.addEventListener('storage', e => checkUserData(e))
+
+    return () => {
+      window.removeEventListener('storage', e => checkUserData(e))
+    }
+  }, [])
+
+  return (
+
+      <UserContext.Provider
+        value={{
+          username,
+          setUsername,
+          accessToken,
+          setAccessToken,
+          loggedIn,
+          setLoggedIn,
+          loading,
+          setLoading,
+        }}
+      >
+          {children}
+      </UserContext.Provider>
+  );
+};
+
+export const useUserContext = () => React.useContext(UserContext);
