b4037f491dc13264f4e86c76a67c729f1bbbde80
israelias <israelias.js@gmail.com>
2021-04-13 13:41:21 +0800

    Add pagination to public explore page

--- frontend/src/pages/explore.tsx
+++ frontend/src/pages/explore.tsx
@@ -1,21 +1,45 @@
+/* eslint-disable no-return-assign */
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-nested-ternary */
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable no-extra-boolean-cast */
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import {
   useQuery,
+  useInfiniteQuery,
+  useQueryClient,
   // useMutation,
   // useQueryClient,
 } from 'react-query';
-// import { QueryResultBase } from '@types/react-query'
+import axios from 'axios';
 import { LoggedinHeader } from '../components/shared/header';
-import { SnippetFeed } from '../components/snippet-feed';
 import { Search } from '../components/snippet_crud/search-form';
 // import { ToUserButton } from '../components/shared/special-button'
 import { getRequest } from '../lib/fetcher';
 import { useUserContext } from '../context/user.context';
+import { isError } from '../lib/isError';
+import { FormPage } from '../components/layout/comonForm';
+import {
+  MainFeed,
+  Container as MainContainer,
+} from '../components/layout/commonCard';
+import useIntersectionObserver from '../lib/useIntersect';
 
-function isError(error: unknown): error is Error {
-  return error instanceof Error;
+interface FetchProps {
+  page: number;
+  tag: string;
 }
+const fetchSnippets = async ({
+  page = 1,
+  tag = '',
+}: FetchProps) => {
+  const { data } = await axios.get(
+    `http://localhost:5000/api/snippets?page=${page}&tags=${tag}`
+  );
+  return data;
+};
 
 interface ExploreProps
   extends RouteComponentProps<{ id: string }> {
@@ -27,63 +51,102 @@ export const Explore: React.FC<ExploreProps> = ({
   history,
   match,
 }) => {
-  const [searchTerm, setSearchTerm] = React.useState('');
-  const [searchBy, setSearchBy] = React.useState<SearchBy>(
-    'title'
-  );
-  const [tagId, setTagId] = React.useState('');
-  const [usernameId, setUsernameId] = React.useState('');
+  const queryClient = useQueryClient();
   const user = useUserContext();
   const username = user!.username ? user!.username : 'joem';
+  const [tagParam, setTagParam] = React.useState('');
+  const [pageParam, setPageParam] = React.useState(1);
+  // const [currentTag, setCurrentTag] = React.useState('');
+  // const onTagParamClick = (tag: string) => setTagParam(tag);
 
-  const onTagIdClick = () => setTagId(tagId);
-  const onUsernameIdClick = () => setUsernameId(usernameId);
-  const { isLoading, error, status, data } = useQuery(
-    'allSnippets',
-    () =>
-      getRequest({
-        url: `api/snippets`,
-        accessToken: user!.accessToken,
-      })
+  const {
+    error,
+    status,
+    data,
+    isFetching,
+    isPreviousData,
+  } = useQuery(
+    ['snippets', pageParam, tagParam],
+    () => fetchSnippets({ page: pageParam, tag: tagParam }),
+    {
+      keepPreviousData: true,
+      staleTime: 5000,
+    }
   );
-  if (isLoading) return <p>Loading...</p>;
 
-  if (status === 'error') {
-    if (isError(error)) {
-      return (
-        <p>{`An error has occurred: ${error.message}`}</p>
+  React.useEffect(() => {
+    if (data?.meta.has_next) {
+      queryClient.prefetchQuery(
+        ['snippets', pageParam + 1],
+        () =>
+          fetchSnippets({
+            page: pageParam + 1,
+            tag: tagParam,
+          })
       );
     }
-  }
+  }, [data, pageParam, tagParam, queryClient]);
 
-  return (
+  const loadMoreButtonRef = React.useRef<HTMLButtonElement>(
+    null
+  );
+  useIntersectionObserver(loadMoreButtonRef, {
+    enabled: data?.meta.has_next,
+    // onIntersect: fetchNextPage,
+    onIntersect: () => {
+      setPageParam((p) => p + 1);
+      queryClient.prefetchQuery(
+        ['snippets', pageParam + 1],
+        () =>
+          fetchSnippets({
+            page: pageParam + 1,
+            tag: tagParam,
+          })
+      );
+    },
+  });
+  // const isVisible = !!entry?.isIntersecting;
+
+  return status === 'loading' ? (
+    <p>Loading...</p>
+  ) : status === 'error' && isError(error) ? (
+    <p>Error: {error.message}</p>
+  ) : (
     <div>
-      <LoggedinHeader loggedIn={true} username={username} />
-      <Search
-        query="query"
-        searchBy={searchBy}
-        onSearchByChange={() => setSearchBy(searchBy)}
-        searchTerm={searchTerm}
-        onSearchTermChange={() => setSearchTerm(searchTerm)}
-      />
-      <button
-        type="button"
-        onClick={() => history.push('/snippets/all')}
-      >
-        All
-      </button>
-      <h1>
-        Searching for {searchTerm} by {searchBy}
-      </h1>
+      <MainContainer>
+        <MainFeed snippets={data?.items} />
+      </MainContainer>
 
-      <SnippetFeed
-        setTagId={onTagIdClick}
-        setUsernameId={onUsernameIdClick}
-        searchBy={searchBy}
-        searchTerm={searchTerm}
-        snippets={data}
-        username={username}
-      />
+      <div>
+        <div>
+          Current Page:
+          {pageParam + 1}
+        </div>
+        <button
+          type="button"
+          onClick={() =>
+            setPageParam((old) => Math.max(old - 1, 0))
+          }
+          disabled={pageParam === 0}
+        >
+          Previous
+        </button>
+        <button
+          type="button"
+          ref={loadMoreButtonRef}
+          // onClick={() => setPageParam((p) => p + 1)}
+          onClick={() => {
+            setPageParam((old) =>
+              data?.meta.has_next ? old + 1 : old
+            );
+          }}
+          disabled={isPreviousData || !data?.meta.has_next}
+        >
+          Next Page
+        </button>
+
+        <div>{isFetching ? 'Fetching...' : null}</div>
+      </div>
     </div>
   );
 };
