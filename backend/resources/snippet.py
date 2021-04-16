d33c08ec1597730c8bc6792ca4b8854f3e6c3462
israelias <israelias.js@gmail.com>
2021-04-16 18:53:22 +0800

    Rewrite snippet api to receive pagination and tag requests

--- backend/resources/snippet.py
+++ backend/resources/snippet.py
@@ -1,11 +1,14 @@
 from flask import Response, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from database.models import Snippet, User, Collection
-from flask_restful import Resource, fields, marshal_with
+from flask_restful import Resource, marshal_with
 from bson import ObjectId
 import datetime
 from flask_mongoengine.wtf import model_form
 
+from flask_restful.fields import Boolean, Nested, Integer, List, String, DateTime, Url
+from flask_restful.reqparse import RequestParser
+from flask_restful.inputs import positive, url
 
 from mongoengine.errors import (
     FieldDoesNotExist,
@@ -23,30 +26,109 @@ from resources.errors import (
     SnippetNotExistsError,
 )
 
+from database.parsers import (
+    create_widget_reqparser,
+    pagination_reqparser,
+    widget_owner_model,
+    widget_model,
+    pagination_links_model,
+    pagination_model,
+    search_reqparser
+)
+
+from services.pagination_service import (
+    # create_widget, 
+    retrieve_widget_list,
+    retrieve_widget
+)
 
 class SnippetsApi(Resource):
+    """Handles HTTP requests to URL: api/snippets."""
+    
+    # @jwt_required()
     def get(self):
-        snippets = []
-        for doc in Snippet.objects():
-            snippets.append(
+        """Retrieve a list of public snippets."""
+        # /snippets?key1=val1&key2=val2
+        # http :5000/api/v1/snippets?page=1&per_page=10
+        request_data = pagination_reqparser.parse_args()
+        # page = int(request_data.get("page", 1))
+        page = request_data["page"]
+        # per_page = int(request_data.get("per_page", 10))
+        per_page = request_data["per_page"]
+        # return retrieve_widget_list(page, per_page)
+        # tags = request_data["tags"]
+        # myTags = request.args.get('tags', ['java'])
+        
+        # search_data = search_reqparser.parse_args()
+        # search_tags = search_data["tags"]
+        
+        allTags = []
+        allSnips = Snippet.objects()
+        for tag in Snippet.objects():
+            for k in tag['tags']:
+                if k not in allTags:
+                    allTags.append(k)
+        
+        searchParser = RequestParser(bundle_errors=True)
+        searchParser.add_argument('tags', type=str, required=False)
+        searchArgs = searchParser.parse_args()
+        testTags = searchArgs['tags']
+        testTagsArr = [testTags]
+        
+        # parser = RequestParser()
+        # parser.add_argument('key1', type=str)
+        # parser.add_argument('key2', type=str)
+        # parser.add_argument('page', type=positive, required=False, default=1 )
+        # parser.add_argument('per_page', type=positive, required=False, choices=[5, 10, 25, 50, 100], default=10)
+        # return parser.parse_args()
+        
+        # pagination = Pagination()
+        # page = int(request.args.get('page',1))
+        # limit = int(request.args.get('limit',10))
+        
+        paginated = Snippet.objects(
+            tags__in=testTagsArr if testTags else allTags,
+            ).order_by(
+                'title'
+                ).paginate(
+                page=page, 
+                per_page=per_page, 
+                )    
+            
+        meta = {
+            "has_prev": paginated.has_prev,
+            "has_next": paginated.has_next,
+            "page": paginated.page,
+            "total_pages": paginated.pages,
+            "items_per_page": paginated.per_page,
+            "total_items": paginated.total,
+        }
+        
+        # items = jsonify()
+        
+        resp = {
+            "meta": meta,
+            "items": [
                 {
-                    "_id": str(ObjectId(doc["id"])),
-                    "title": doc["title"],
-                    "filename": doc["filename"],
-                    "description": doc["description"],
-                    "language": doc["language"],
-                    "value": doc["value"],
-                    "addedBy": doc["addedBy"]["username"],
-                    "likedBy": [elem["username"] for elem in doc["likedBy"]],
-                    "tags": doc["tags"],
-                    "addedOn": doc["addedOn"],
-                    "updatedOn": doc["updatedOn"],
-                    "private": doc["private"],
-                    "source": doc["source"],
+                    "_id": str(ObjectId(k["id"])),
+                    "title": k["title"],
+                    "filename": k["filename"],
+                    "description": k["description"],
+                    "language": k["language"],
+                    "value": k["value"],
+                    "addedBy": k["addedBy"]["username"],
+                    "likedBy": [elem["username"] for elem in k["likedBy"]],
+                    "tags": k["tags"],
+                    "addedOn": k["addedOn"],
+                    "updatedOn": k["updatedOn"],
+                    "private": k["private"],
+                    "source": k["source"],
                 }
-            )
+                for k in paginated.items
+            ]
+        }
 
-        return jsonify(snippets)
+        return jsonify(resp)
 
     @jwt_required()
     def post(self):
@@ -70,6 +152,14 @@ class SnippetsApi(Resource):
         except Exception as e:
             raise InternalServerError
 
+
+# class SnippetSearchApi(Resource):
+#     """Handles HTTP requests to URL: /snippets/{name}."""
+#     @marshal_with(widget_model)
+#     def get(self, name):
+#         """Retrieve a widget."""
+#         return retrieve_widget(name)
+    
 
 class SnippetApi(Resource):
     def get(self, id):
@@ -104,7 +194,8 @@ class SnippetApi(Resource):
     def put(self, id):
         try:
             user_id = get_jwt_identity()
-            snippet = Snippet.objects.get(id=id, addedBy=user_id)
+            username = User.objects.get(username=user_id)
+            snippet = Snippet.objects.get(id=id, addedBy=username)
             body = request.get_json()
             now = datetime.datetime.now(datetime.timezone.utc)
             snippet.update(**body, updatedOn=now)
