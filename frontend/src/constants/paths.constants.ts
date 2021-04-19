09aac2cf19fb920554b360eb4e9eea6508668829
israelias <israelias.js@gmail.com>
2021-04-20 02:27:19 +0800

    Add routes to constants directory

---
+++ frontend/src/constants/paths.constants.ts
@@ -0,0 +1,7 @@
+export const PATHS = [
+  { label: 'Collections', path: '/' },
+  { label: 'Faves', path: '/faves' },
+  { label: 'Explore', path: '/explore' },
+
+  { label: 'Add', path: '/add' },
+];
