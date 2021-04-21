8542754340c5d2162eeff4c561830c90d9265918
israelias <israelias.js@gmail.com>
2021-04-21 17:10:35 +0800

    Destructure shareable UI to designated directory

---
+++ frontend/src/components/shared/motion-box.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { HTMLChakraProps, chakra } from '@chakra-ui/react';
+
+import { motion, HTMLMotionProps } from 'framer-motion';
+
+type Merge<P, T> = Omit<P, keyof T> & T;
+
+type MotionBoxProps = Merge<
+  HTMLChakraProps<'div'>,
+  HTMLMotionProps<'div'>
+>;
+
+export const MotionBox: React.FC<MotionBoxProps> = motion(chakra.div);
