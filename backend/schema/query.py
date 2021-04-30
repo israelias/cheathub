50aa48a7d1e76c3fe1145ae27fca269d0762aef6
israelias <israelias.js@gmail.com>
2021-04-30 19:21:26 +0800

    Remove graphql query submodule from backend

--- backend/schema/query.py
+++
@@ -1,5 +0,0 @@
-from .models import Query as AuthQuery, Mutation
-from .schema import Query as DataQuery, User, Snippet
-
-auth_schema = graphene.Schema(query=AuthQuery, mutation=Mutation)
-data_schema = graphene.Schema(query=DataQuery, types=[User, Snippet])
