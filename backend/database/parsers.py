02b1a3a7b9fb84bfd20e59a67224cdad0cd77831
israelias <israelias.js@gmail.com>
2021-04-30 19:28:06 +0800

    Define separate parsers for each request scope

--- backend/database/parsers.py
+++ backend/database/parsers.py
@@ -1,54 +1,63 @@
 from flask_restful import Resource
-from flask_restful.fields import Boolean, Nested, Integer, List, String, DateTime, Url, Raw
+from flask_restful.fields import (
+    Boolean,
+    Nested,
+    Integer,
+    List,
+    String,
+    DateTime,
+    Url,
+    Raw,
+)
 from flask_restful.reqparse import RequestParser
 from flask_restful.inputs import positive, url, regex
 
-pagination_reqparser = RequestParser(bundle_errors=True)
-pagination_reqparser.add_argument(
-    "page", type=positive, required=False, default=1
+pagination_parser = RequestParser(bundle_errors=True)
+pagination_parser.add_argument(
+    "page",
+    type=positive,
+    required=False,
+    default=1,
 )
-pagination_reqparser.add_argument(
-    "per_page", type=positive, required=False, choices=[5, 10, 25, 50, 100], default=10
-)
-
-search_reqparser = RequestParser(bundle_errors=True)
-
-regexArray = [regex]
-search_reqparser.add_argument(
-    "tags", type=str, required=False
+pagination_parser.add_argument(
+    "per_page", type=positive, required=False, choices=[5, 10, 25, 50, 100], default=25
 )
 
-widget_owner_model =  {"email": String, "username": String}
+search_parser = RequestParser(bundle_errors=True)
+search_parser.add_argument(
+    "query",
+    type=str,
+    required=False,
+    # nullable=True,
+    store_missing=True,
+    # default="hello world",
+)
 
-widget_model = {
-        "title": String,
-        "filename": String,
-        "addedOn": DateTime(attribute="addedOn"),
-        "language": String,
-        "description": String,
-        "value": String,
-        "active": Boolean,
-        "owner": Nested(widget_owner_model),
-        # "source": Url,
-        # "links": Url("api.snippets")
-        # "link": "api/snippets" 
-    }
+tag_parser = RequestParser(bundle_errors=True)
+tag_parser.add_argument(
+    "tags",
+    type=str,
+    action="append",
+    trim=True,
+    required=False,
+    store_missing=True,
+    # nullable=True,
+)
 
+language_parser = RequestParser(bundle_errors=True)
+language_parser.add_argument(
+    "language",
+    type=str,
+    required=False,
+    # nullable=True,
+    # default="javascript",
+    store_missing=True,
+)
 
-pagination_links_model = {"self": String, "prev": String, "next": String, "first": String, "last": String},
+user_parser = RequestParser(bundle_errors=True)
+user_parser.add_argument("author", type=str, required=False)
+user_parser.add_argument("liked_by", type=str, required=False)
 
-
-
-pagination_model = {
-        "links": Nested(pagination_links_model, allow_null=True),
-        "has_prev": Boolean,
-        "has_next": Boolean,
-        "page": Integer,
-        "total_pages": Integer(attribute="pages"),
-        "items_per_page": Integer(attribute="per_page"),
-        "total_items": Integer(attribute="total"),
-        "items": List(Nested(widget_model)),
-    }
 
 create_widget_reqparser = RequestParser(bundle_errors=True)
 create_widget_reqparser.add_argument(
@@ -75,4 +84,3 @@ create_widget_reqparser.add_argument(
 )
 update_widget_reqparser = create_widget_reqparser.copy()
 update_widget_reqparser.remove_argument("name")
-
