a30ddc8449d08c782e4cada5dd43dd6dd0e7acdc
israelias <israelias.js@gmail.com>
2021-04-30 19:44:18 +0800

    Modify public endpoint to return pagination and self-URLs

--- backend/resources/snippet.py
+++ backend/resources/snippet.py
@@ -1,10 +1,11 @@
 from flask import Response, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from database.models import Snippet, User, Collection
-from flask_restful import Resource, marshal_with
+from flask_restful import Resource, marshal_with, url_for
 from bson import ObjectId
 import datetime
 from flask_mongoengine.wtf import model_form
+from flask_mongoengine import Pagination
 
 from flask_restful.fields import Boolean, Nested, Integer, List, String, DateTime, Url
 from flask_restful.reqparse import RequestParser
@@ -27,87 +28,60 @@ from resources.errors import (
 )
 
 from database.parsers import (
-    create_widget_reqparser,
-    pagination_reqparser,
-    widget_owner_model,
-    widget_model,
-    pagination_links_model,
-    pagination_model,
-    search_reqparser
+    pagination_parser,
+    search_parser,
+    tag_parser,
+    language_parser,
 )
 
-from services.pagination_service import (
-    # create_widget, 
-    retrieve_widget_list,
-    retrieve_widget
+from services.pagination_service import pagination_meta, pagination_links
+
+from database.constants import (
+    all_languages,
+    all_tags,
+    # all_fans,
+    # all_usernames
 )
+
 
 class SnippetsApi(Resource):
     """Handles HTTP requests to URL: api/snippets."""
-    
+
     # @jwt_required()
     def get(self):
         """Retrieve a list of public snippets."""
-        # /snippets?key1=val1&key2=val2
-        # http :5000/api/v1/snippets?page=1&per_page=10
-        request_data = pagination_reqparser.parse_args()
-        # page = int(request_data.get("page", 1))
-        page = request_data["page"]
-        # per_page = int(request_data.get("per_page", 10))
-        per_page = request_data["per_page"]
-        # return retrieve_widget_list(page, per_page)
-        # tags = request_data["tags"]
-        # myTags = request.args.get('tags', ['java'])
-        
-        # search_data = search_reqparser.parse_args()
-        # search_tags = search_data["tags"]
-        
-        allTags = []
-        allSnips = Snippet.objects()
-        for tag in Snippet.objects():
-            for k in tag['tags']:
-                if k not in allTags:
-                    allTags.append(k)
-        
-        searchParser = RequestParser(bundle_errors=True)
-        searchParser.add_argument('tags', type=str, required=False)
-        searchArgs = searchParser.parse_args()
-        testTags = searchArgs['tags']
-        testTagsArr = [testTags]
-        
-        # parser = RequestParser()
-        # parser.add_argument('key1', type=str)
-        # parser.add_argument('key2', type=str)
-        # parser.add_argument('page', type=positive, required=False, default=1 )
-        # parser.add_argument('per_page', type=positive, required=False, choices=[5, 10, 25, 50, 100], default=10)
-        # return parser.parse_args()
-        
-        # pagination = Pagination()
-        # page = int(request.args.get('page',1))
-        # limit = int(request.args.get('limit',10))
-        
-        paginated = Snippet.objects(
-            tags__in=testTagsArr if testTags else allTags,
-            ).order_by(
-                'title'
-                ).paginate(
-                page=page, 
-                per_page=per_page, 
-                )    
-            
-        meta = {
+
+        tag_request = tag_parser.parse_args()
+        tags = tag_request["tags"]
+
+        page_request = pagination_parser.parse_args()
+        page = page_request["page"]
+        per_page = page_request["per_page"]
+
+        all_tags_arr = all_tags()
+
+        paginated = (
+            Snippet.objects(
+                tags__in=tags if tags else all_tags_arr,
+            )
+            .order_by("-addedOn")
+            .paginate(
+                page=page,
+                per_page=per_page,
+            )
+        )
+
+        meta = pagination_meta(paginated)
+        links = pagination_links(paginated, "snippetsapi")
+
+        resp = {
+            "links": links,
             "has_prev": paginated.has_prev,
             "has_next": paginated.has_next,
             "page": paginated.page,
             "total_pages": paginated.pages,
             "items_per_page": paginated.per_page,
             "total_items": paginated.total,
-        }
-        
-        # items = jsonify()
-        
-        resp = {
-            "meta": meta,
             "items": [
                 {
                     "_id": str(ObjectId(k["id"])),
@@ -123,11 +97,11 @@ class SnippetsApi(Resource):
                     "updatedOn": k["updatedOn"],
                     "private": k["private"],
                     "source": k["source"],
+                    "url": url_for("snippetapi", id=str(ObjectId(k["id"]))),
                 }
                 for k in paginated.items
-            ]
+            ],
         }
-
         return jsonify(resp)
 
     @jwt_required()
@@ -153,38 +127,31 @@ class SnippetsApi(Resource):
             raise InternalServerError
 
 
-# class SnippetSearchApi(Resource):
-#     """Handles HTTP requests to URL: /snippets/{name}."""
-#     @marshal_with(widget_model)
-#     def get(self, name):
-#         """Retrieve a widget."""
-#         return retrieve_widget(name)
-    
-
 class SnippetApi(Resource):
     def get(self, id):
         try:
-            snippet = []
-            for doc in Snippet.objects(id=id):
-                snippet.append(
-                    {
-                        "_id": str(ObjectId(doc["id"])),
-                        "title": doc["title"],
-                        "filename": doc["filename"],
-                        "description": doc["description"],
-                        "language": doc["language"],
-                        "value": doc["value"],
-                        "addedBy": doc["addedBy"]["username"],
-                        "likedBy": [elem["username"] for elem in doc["likedBy"]],
-                        "tags": doc["tags"],
-                        "addedOn": doc["addedOn"],
-                        "updatedOn": doc["updatedOn"],
-                        "private": doc["private"],
-                        "source": doc["source"],
-                    }
-                )
+            snippet = Snippet.objects(id=id)
+            response = [
+                {
+                    "_id": str(ObjectId(doc["id"])),
+                    "title": doc["title"],
+                    "filename": doc["filename"],
+                    "description": doc["description"],
+                    "language": doc["language"],
+                    "value": doc["value"],
+                    "addedBy": doc["addedBy"]["username"],
+                    "likedBy": [elem["username"] for elem in doc["likedBy"]],
+                    "tags": doc["tags"],
+                    "addedOn": doc["addedOn"],
+                    "updatedOn": doc["updatedOn"],
+                    "private": doc["private"],
+                    "source": doc["source"],
+                    "score": doc["score"],
+                }
+                for doc in snippet
+            ]
 
-            return jsonify(snippet)
+            return jsonify(response)
         except DoesNotExist:
             raise SnippetNotExistsError
         except Exception:
@@ -212,7 +179,8 @@ class SnippetApi(Resource):
     def delete(self, id):
         try:
             user_id = get_jwt_identity()
-            snippet = Snippet.objects.get(id=id, addedBy=user_id)
+            username = User.objects.get(username=user_id)
+            snippet = Snippet.objects.get(id=id, addedBy=username)
             snippet.delete()
             return {"message": "Snippet deleted"}, 200
         except DoesNotExist:
@@ -225,9 +193,25 @@ class LikeSnippetApi(Resource):
     @jwt_required()
     def post(self, id):
         user_id = get_jwt_identity()
+        user = User.objects.get(username=user_id)
         snippet = Snippet.objects.get(id=id)
-        snippet.update(push__likedBy=user_id)
-        snippet.save()
-        user_id.update(push__snippets_liked=snippet)
-        user_id.save()
-        return {"message": "Snippet liked"}, 200
+        if user in snippet.likedBy:
+            print("existing")
+            snippet.update(pull__likedBy=user, dec__score=1)
+            snippet.save()
+            snippet.reload()
+            user.update(pull__snippets_liked=snippet)
+            user.save()
+            user.reload()
+            print("snippet unfaved")
+            return {"message": "Snippet unfaved"}, 200
+        else:
+            print("new")
+            snippet.update(push__likedBy=user, inc__score=1)
+            snippet.save()
+            snippet.reload()
+            user.update(push__snippets_liked=snippet)
+            user.save()
+            user.reload()
+            print("snippet faved")
+            return {"message": "Snippet faved"}, 200
