91340419a0adde253443d40c36c9c2fceda7d000
israelias <israelias.js@gmail.com>
2021-05-12 06:22:33 +0800

    Add editor controls to viewer directory

---
+++ frontend/src/components/snippet/editor/controls.tsx
@@ -0,0 +1,140 @@
+import React from 'react';
+import { useHistory, Link as RouterLink } from 'react-router-dom';
+import {
+  FormLabel,
+  Box,
+  Tooltip,
+  Menu,
+  MenuItem,
+  MenuList,
+  MenuButton,
+  VStack,
+  HStack,
+  Button,
+  IconButton,
+  Link,
+  Icon,
+  useBoolean,
+  useClipboard,
+  useColorModeValue as mode,
+  ButtonGroup,
+} from '@chakra-ui/react';
+import { CheckIcon, CloseIcon, CopyIcon } from '@chakra-ui/icons';
+import {
+  GoPlus,
+  GoPlusSmall,
+  GoDash,
+  GoLinkExternal,
+  GoLink,
+  GoPencil,
+  GoDiffIgnored,
+  GoDiffModified,
+} from 'react-icons/go';
+import { TimeAgo } from '../../shared/time';
+import { MotionUl, MotionLi, MotionP } from '../../shared/motion-box';
+
+export const ViewerControls: React.FC<{
+  value: string;
+  lineNumbers: boolean;
+  wrapLines: boolean;
+  setWrapLines: {
+    readonly on: () => void;
+    readonly off: () => void;
+    readonly toggle: () => void;
+  };
+  setLineNumbers: {
+    readonly on: () => void;
+    readonly off: () => void;
+    readonly toggle: () => void;
+  };
+}> = ({
+  value,
+  lineNumbers,
+  wrapLines,
+  setLineNumbers,
+  setWrapLines,
+}) => (
+  <HStack>
+    <ButtonGroup size="xs" isAttached variant="outline">
+      <Button
+        onClick={setLineNumbers.toggle}
+        mr="-px"
+        fontWeight="light"
+        bg={mode('#fafafa', '#252945')}
+        borderColor={mode('#d8d9da', '#7e88c3')}
+        color={mode('#252945', '#fafafa')}
+        _hover={{ bg: mode('#f5f2f0', '#373B53') }}
+        rightIcon={
+          lineNumbers ? (
+            <Icon fontSize="12px" as={GoDiffModified} />
+          ) : (
+            <Icon fontSize="12px" as={GoDiffIgnored} />
+          )
+        }
+      >
+        {lineNumbers ? 'Line numbers on' : 'Line numbers off'}
+      </Button>
+
+      <Button
+        onClick={setWrapLines.toggle}
+        mr="-px"
+        fontWeight="light"
+        bg={mode('#fafafa', '#252945')}
+        borderColor={mode('#d8d9da', '#7e88c3')}
+        color={mode('#252945', '#fafafa')}
+        _hover={{ bg: mode('#f5f2f0', '#373B53') }}
+        rightIcon={
+          <Icon
+            fontSize="10px"
+            as={wrapLines ? GoDiffModified : GoDiffIgnored}
+          />
+        }
+      >
+        {wrapLines ? 'Line wrap on' : 'Line wrap off'}
+      </Button>
+
+      <CopyButton editing value={value} />
+    </ButtonGroup>
+  </HStack>
+);
+
+export const CopyButton: React.FC<{
+  editing?: boolean;
+  value: string;
+}> = ({ editing, value }) => {
+  const { hasCopied, onCopy } = useClipboard(value);
+  return editing ? (
+    <Button
+      variant="outline"
+      size="xs"
+      mr="-px"
+      fontWeight="light"
+      bg={mode('#fafafa', '#252945')}
+      borderColor={mode('#d8d9da', '#7e88c3')}
+      color={mode('#252945', '#fafafa')}
+      _hover={{ bg: mode('#f5f2f0', '#373B53') }}
+      rightIcon={
+        hasCopied ? (
+          <CheckIcon fontSize="10px" />
+        ) : (
+          <CopyIcon fontSize="10px" />
+        )
+      }
+    >
+      {hasCopied ? 'Copied' : 'Copy'}
+    </Button>
+  ) : (
+    <IconButton
+      variant="outline"
+      color="gray.400"
+      bg="rgb(255,255,255, .3)"
+      size="xs"
+      aria-label="Copy snippet"
+      onClick={onCopy}
+      icon={hasCopied ? <CheckIcon /> : <CopyIcon />}
+      position="absolute"
+      right="36px"
+      mt="10px"
+    />
+  );
+};
