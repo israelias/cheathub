72fe1f5a184938dc98047767a1a5d3cbc04e5cf6
israelias <israelias.js@gmail.com>
2021-05-12 06:01:04 +0800

    Add collection starter package for new users in auth.py

--- backend/resources/auth.py
+++ backend/resources/auth.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, jsonify
 from flask_jwt_extended import (
     create_access_token,
     create_refresh_token,
@@ -6,7 +6,7 @@ from flask_jwt_extended import (
     jwt_required,
 )
 
-from database.models import User
+from database.models import User, Snippet, Collection
 from database.models import TokenBlocklist
 
 from flask_restful import Resource
@@ -26,18 +26,76 @@ class SignupApi(Resource):
     def post(self):
         try:
             body = request.get_json()
-            # @TODO Fix the exception error on exitinguser validation
-            # existing = User.objects.get(username=body.get('username'))
-            # if existing:
-            #     raise UsernameAlreadyExistsError
             user = User(**body)
+
             user.hash_password()
             user.save()
+            user.reload()
+
+            now = datetime.datetime.now(datetime.timezone.utc)
 
             id = user.id
             username = user.username
+            # Required for assigned an owner to snippets
+            saved_user = User.objects.get(username=username)
 
-            expires = datetime.timedelta(minutes=15)
+            snippet_py = Snippet(
+                title="{}.py".format(username),
+                tags=["first post"],
+                description="From Cheat-Hub",
+                language="python",
+                value="print('hello {}')".format(username),
+                addedBy=saved_user,
+                addedOn=now,
+            )
+
+            snippet_js = Snippet(
+                title="{}.js".format(username),
+                tags=["first post"],
+                description="From Cheat-Hub",
+                language="javascript",
+                value="console.log('hello {}');".format(username),
+                addedBy=saved_user,
+                addedOn=now,
+            )
+
+            snippet_sh = Snippet(
+                title="{}.sh".format(username),
+                tags=["first post"],
+                description="From Cheat-Hub",
+                language="bash",
+                value="#!/bin/bash\n\necho 'hello {}'".format(username),
+                addedBy=saved_user,
+                addedOn=now,
+            )
+
+            snippet_py.save()
+            snippet_py.reload()
+            snippet_js.save()
+            snippet_js.reload()
+            snippet_sh.save()
+            snippet_sh.reload()
+            # snippet.update(set__addedBy=user)
+
+            # snippet.save()
+            # snippet.reload()
+            user.update(push_all__snippets_created=[snippet_py, snippet_js, snippet_sh])
+            user.save()
+            user.reload()
+
+            collection = Collection(
+                name="Greetings {}".format(username),
+                snippets=[snippet_py, snippet_js, snippet_sh],
+                date=now,
+                owner=user,
+            )
+
+            collection.save()
+
+            user.update(push__collections=collection)
+            user.save()
+
+            expires = datetime.timedelta(hours=3)
             access_token = create_access_token(
                 identity=str(username), expires_delta=expires
             )
@@ -47,17 +105,22 @@ class SignupApi(Resource):
             refresh_cookie = [("Set-Cookie", "refresh_token={}".format(refresh_token))]
 
             return (
-                {"access_token": access_token, "username": username},
+                {
+                    "access_token": access_token,
+                    "username": username,
+                },
                 200,
                 refresh_cookie,
             )
 
         except FieldDoesNotExist:
-            raise SchemaValidationError
+            return {"message": "Request is missing required fields."}, 400
+
         except NotUniqueError:
-            raise EmailAlreadyExistsError
+            return {"message": "User with given email address already exists."}, 401
+
         except Exception as e:
-            raise InternalServerError
+            return {"message": "Something went wrong."}, 500
 
 
 class LoginApi(Resource):
@@ -82,15 +145,20 @@ class LoginApi(Resource):
             refresh_cookie = [("Set-Cookie", "refresh_token={}".format(refresh_token))]
 
             return (
-                {"access_token": access_token, "username": user.username},
+                {
+                    "access_token": access_token,
+                    "username": user.username,
+                },
                 200,
                 refresh_cookie,
             )
 
         except (UnauthorizedError, DoesNotExist):
-            raise UnauthorizedError
+            return {"message": "Invalid username or password."}, 401
+            # raise UnauthorizedError
         except Exception as e:
-            raise InternalServerError
+            return {"message": "Something went wrong."}, 500
+            # raise InternalServerError
 
 
 class LogoutApi(Resource):
