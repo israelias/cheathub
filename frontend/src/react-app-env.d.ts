3c2cb8e06a6ea37349152e7933399f8484b5b247
israelias <israelias.js@gmail.com>
2021-05-12 23:40:18 +0800

    Prepare react declarations for production

--- frontend/src/react-app-env.d.ts
+++ frontend/src/react-app-env.d.ts
@@ -1,15 +1,1 @@
 /// <reference types="react-scripts" />
-
-// declare namespace NodeJS {
-//   interface ProcessEnv {
-//     NODE_ENV: 'development' | 'production' | 'test';
-//     PUBLIC_URL: string;
-//     REACT_APP_HASH: string;
-//     REACT_APP_API_URI: string;
-//     REACT_APP_WS_URI: string;
-//     PUBLIC_API: string;
-//   }
-// }
-// interface Window {
-//   Stripe: any;
-// }
