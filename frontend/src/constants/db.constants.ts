adee9536873e7508b1dda3aff53107c320a077dd
israelias <israelias.js@gmail.com>
2021-05-12 15:33:08 +0800

    Add constant database ids to fetch welcome collection

---
+++ frontend/src/constants/db.constants.ts
@@ -0,0 +1,17 @@
+export const PR_HELLO = {
+  _id: '609a5aed6c4d5f29aefa65ca',
+  name: 'Hello World',
+  owner: 'Cheat-Hub',
+};
+
+export const PR_TOUR_COLLECTION_01 = {
+  _id: '606a97263e434d9fa1ab7f34',
+  name: 'Language Intros',
+  owner: 'Cheat-Hub',
+};
+
+export const PR_TOUR_COLLECTION_02 = {
+  _id: '606a97253e434d9fa1ab7f32',
+  name: 'Typescript Event Handlers',
+  owner: 'Cheat-Hub',
+};
